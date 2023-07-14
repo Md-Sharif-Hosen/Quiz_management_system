9519443bac5edcf0cd4b2b122a7a9c441bb1935f
sharif-hosen <mdsharifkhan762@gmail.com>
2023-07-14 10:04:39 +0600

    admin dashboard new add

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,21 +1,21 @@
 <aside class="main-sidebar">
     <!-- sidebar-->
     <section class="sidebar position-relative">
-	  	<div class="multinav">
-		  <div class="multinav-scroll" style="height: 100%;">
-			  <!-- sidebar menu-->
-			  <ul class="sidebar-menu" data-widget="tree">
-				<li class="header">Dashboard & Apps</li>
-				<li class="treeview">
-				  <a href="#">
-					<i class="icon-Layout-4-blocks"><span class="path1"></span><span class="path2"></span></i>
-					<span>Dashboard</span>
-					<span class="pull-right-container">
-					  <i class="fa fa-angle-right pull-right"></i>
-					</span>
-				  </a>
-				</li>
-				{{-- <li class="treeview">
+        <div class="multinav">
+            <div class="multinav-scroll" style="height: 100%;">
+                <!-- sidebar menu-->
+                <ul class="sidebar-menu" data-widget="tree">
+                    <li class="header">Dashboard & Apps</li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="icon-Layout-4-blocks"><span class="path1"></span><span class="path2"></span></i>
+                            <span>Dashboard</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-right pull-right"></i>
+                            </span>
+                        </a>
+                    </li>
+                    {{-- <li class="treeview">
 				  <a href="#">
 					<i span class="icon-Layout-grid"><span class="path1"></span><span class="path2"></span></i>
 					<span>Apps</span>
@@ -31,17 +31,28 @@
 					<li><a href="mailbox.html"><i class="icon-Commit"><span class="path1"></span><span class="path2"></span></i>Mailbox</a></li>
 				  </ul>
 				</li> --}}
-                <li class="treeview">
-                    <a href="{{ route('quizz') }}">
-                      <i span class="icon-Layout-grid"><span class="path1"></span><span class="path2"></span></i>
-                      <span>Quizzes</span>
-                      <span class="pull-right-container">
-                        <i class="fa fa-angle-right pull-right"></i>
-                      </span>
-                    </a>
-                  </li>
-			  </ul>
-		  </div>
-		</div>
+                    <li class="treeview">
+                        <a href="{{ route('quizz') }}">
+                            <i span class="icon-Layout-grid"><span class="path1"></span><span
+                                    class="path2"></span></i>
+                            <span>Quizzes</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-right pull-right"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="">
+                            <i span class="icon-Layout-horizontal"><span class="path1"></span><span
+                                    class="path2"></span></i>
+                            <span>Classes</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-right pull-right"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </section>
-  </aside>
+</aside>
