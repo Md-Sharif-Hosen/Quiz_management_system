25164fb15dbd816bc882f9b8567637c6ddb5ab3c
Md-Sharif-Hosen <mdsharifkhan762@gmail.com>
2023-09-02 13:19:55 +0600

    dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\QuizResult;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -11,7 +13,10 @@ class AdminController extends Controller
     public function index()
     {
         //function_body
-        return view('admin.dashboardhome');
+        $all_student=User::where('role_id',2)->count();
+        $attend_quiz=QuizResult::select('user_id')->pluck('user_id')->unique()->count('user_id');
+        // dd($attend_quiz);
+        return view('admin.dashboardhome',compact('all_student','attend_quiz'));
     }
     public function profile()
     {
